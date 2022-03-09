bf194620c4e3bbf510c6354564394cb36c46b919
taserbeat <taserbeat@gmail.com>
2022-03-09 21:27:11 +0900

    ライブラリのインストールをスクリプト化

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+readonly VENV_DIR="./.venv/"
+readonly PIPFILE_NAME="Pipfile"
+readonly PIPFILE_LOCK_NAME="Pipfile.lock"
+
 cd $(dirname $0)
 
-if [ -e ./.venv/ ]; then
-  rm -r ./.venv/
+if [ -e ${VENV_DIR} ]; then
+  rm -r ${VENV_DIR}
 fi
 
 export PIPENV_VENV_IN_PROJECT=true
-pipenv sync --dev
+
+if [ -e ${PIPFILE_LOCK_NAME} ]; then
+  echo "Now running 'pipenv sync --dev'"
+  pipenv sync --dev
+elif [ -e ${PIPFILE_NAME} ]; then
+  echo "Now running 'pipenv install --dev'"
+  pipenv install --dev
+else
+  echo "${PIPFILE_NAME} and ${PIPFILE_LOCK_NAME} does not exist."
+fi
